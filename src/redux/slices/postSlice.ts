4f9acf8a6b33e5793ce2fcb47a306d31e9e8c6ee
DerylFeyza <muhammadderylfeyza@gmail.com>
2025-01-10 09:01:59 +0700

    finish: added logic for add post and delete post

--- src/redux/slices/postSlice.ts
+++ src/redux/slices/postSlice.ts
@@ -1,36 +1,41 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 
 const initialState: any = [
-  { id: 1, title: "Post 1", description: "Description of Post 1" },
-  { id: 2, title: "Post 2", description: "Description of Post 2" },
-  { id: 3, title: "Post 3", description: "Description of Post 3" },
+	{ id: 1, title: "Post 1", description: "Description of Post 1" },
+	{ id: 2, title: "Post 2", description: "Description of Post 2" },
+	{ id: 3, title: "Post 3", description: "Description of Post 3" },
 ];
 
 const postsSlice = createSlice({
-  name: "posts",
-  initialState: initialState,
-  reducers: {
-    addPost: () => {
-      // Implementasi logika untuk menambahkan post
-    },
-    updatePost: (state, action: PayloadAction<any>) => {
-      const { id, title, description } = action.payload;
-      const postIndex = state.findIndex((post: any) => post.id === id);
-      if (postIndex !== -1) {
-        state[postIndex].title = title;
-        state[postIndex].description = description;
-      }
-    },
-    deletePost: () => {
-      // Implementasi logika untuk menghapus post
-    },
-  },
+	name: "posts",
+	initialState: initialState,
+	reducers: {
+		addPost: (state, action: PayloadAction<any>) => {
+			const newId = state.length + 1;
+			const { title, description } = action.payload;
+			state.push({ id: newId, title, description });
+		},
+		updatePost: (state, action: PayloadAction<any>) => {
+			const { id, title, description } = action.payload;
+			const postIndex = state.findIndex((post: any) => post.id === id);
+			if (postIndex !== -1) {
+				state[postIndex].title = title;
+				state[postIndex].description = description;
+			}
+		},
+		deletePost: (state, action: PayloadAction<any>) => {
+			const { id } = action.payload;
+			const postIndex = state.findIndex((post: any) => post.id === id);
+			if (postIndex !== -1) {
+				state.splice(postIndex, 1);
+			}
+		},
+	},
 });
 
 export const { addPost, updatePost, deletePost } = postsSlice.actions;
 
 export const selectPostById = (state: any, postId: number) =>
-  state.posts.find((post: any) => post.id === postId);
+	state.posts.find((post: any) => post.id === postId);
 
 export default postsSlice.reducer;
-
